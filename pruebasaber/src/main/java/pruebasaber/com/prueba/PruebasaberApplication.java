ae309ac95fe3e92d8ec3423892fb3bdc6bd77b72
DiegoReyes2826 <diegosanhack28@gmail.com>
2025-04-21 22:13:56 -0500

    Update PruebasaberApplication.java

--- pruebasaber/src/main/java/pruebasaber/com/prueba/PruebasaberApplication.java
+++ pruebasaber/src/main/java/pruebasaber/com/prueba/PruebasaberApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PruebasaberApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PruebasaberApplication.class, args);
     }
